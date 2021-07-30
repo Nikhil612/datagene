c23e47f201206d5a57fb2d0409c16af314f6adb9
Nikhil612 <joshnikhil612@gmail.com>
2021-07-29 20:33:50 -0700

    New Pyhton files

---
+++ requirements.py
@@ -0,0 +1,7 @@
+matplotlib==3.3.2
+h2o==3.32.1.4
+ctgan==1.15.0
+python==3.7.11
+seaborn==0.0.15
+pandas==1.1.5
+numpy==1.19.5
---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages, setup
+
+
+def read_reqs(req_file: str):
+    with open(req_file) as req:
+        return [
+            line.strip()
+            for line in req.readlines()
+            if line.strip() and not line.strip().startswith("#")
+        ]
+
+
+setup(
+    name="datagen",
+    packages=find_packages(),
+    author="Nikhil Joshi",
+    author_email="joshi.nikh@northeastern.edu",
+    url="https://github.com/Nikhil612/datagene",
+    description=(
+        "A Fastest way to generate tabular data and train it on Auto Ml Architecture "
+    ),
+    license="MIT",
+    install_requires=read_reqs("requirements.txt"),
+    include_package_data=True,
+    version="1.0.0",
+    zip_safe=False,
